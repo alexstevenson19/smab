de4a7be7a294536494afaba6580c7d35d57133a9
Alex S <email": null>
2016-08-18 20:09:09 -0500

    cityofchicago api hit, and search with zip, results, aka_name, and risk

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,12 +1,12 @@
-require 'csv'
+# require 'csv'
 
-file = 'food_inspections_fail.csv'
+# file = 'food_inspections_fail.csv'
 
-inspection_fails = []
+# inspection_fails = []
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'food_inspections_fail.csv'))
-# puts csv_text
+# # csv_text = File.read(Rails.root.join('lib', 'seeds', 'food_inspections_fail.csv'))
+# # puts csv_text
 
-CSV.foreach(@file, {headers: true, header_converters: :symbol}) do |row|
-  inspection_fails << row
-end
+# CSV.foreach(@file, {headers: true, header_converters: :symbol}) do |row|
+#   inspection_fails << row
+# end
